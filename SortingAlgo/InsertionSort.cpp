c25e8f5217fcaa02cf1d37d484c5548f773bc4ef
Abhineet Anand <abhisinha656@gmail.com>
2023-04-10 23:45:14 +0530

    update

--- SortingAlgo/InsertionSort.cpp
+++ SortingAlgo/InsertionSort.cpp
@@ -1,6 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+void sort(int arr[], int n)
+{
+    int key, j;
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
 main()
 {
+    int n;
+    cout << "Size of array: " << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter array Elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
